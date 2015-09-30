95210dc2430e1a28bd34fb6440d2b4c189fe351d
Emily Friedman <emilyfriedman@Emilys-MacBook-Pro.local>
2015-09-30 13:47:17 -0700

    initial commit

---
+++ game.js
@@ -0,0 +1,12 @@
+var question1 = prompt("Can I swim?");
+var question2 = prompt("Can I fly?");
+var question3 = prompt("Can I dance?")
+
+
+console.log("swimming response: " + question1);
+console.log("flying response: " + question2);
+console.log("dancing response: " + question3),
+
+alert('your answer to "Can I swim?" was ' + question1);
+alert('your answer to "Can I fly?" was ' + question2);
+alert('your answer to "Can I dance?" was ' + question3);
