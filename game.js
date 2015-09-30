aeb4f263e4765ec389809809428b774519066ddc
Emily Friedman <emilyfriedman@Emilys-MacBook-Pro.local>
2015-09-30 15:41:12 -0700

    added error corrections

--- game.js
+++ game.js
@@ -1,17 +1,17 @@
 var question1 = prompt("Can I swim?");
-if (question1.toUpperCase() ==='YES' || ===' Y') {
+if (question1.toUpperCase() ==='YES' || question1.toUpperCase() === 'Y') {
 	alert('Correct! You never know when you are going to fall into the water.');
 } else {
 	alert('Incorrect, sorry. That\'s a fundamental skill we ALL should know. No?');
 }
 var question2 = prompt("Can I fly?");
-if (question2.toUpperCase() === 'YES' || === 'Y') {
+if (question2.toUpperCase() === 'YES' || question2.toUpperCase() === 'Y') {
 	alert('That\'s right, I am magic !!!');
 } else {
 	alert('Understandable mistake, but I just happen to be lucky like that.');
 }
 var question3 = prompt("Can I dance?")
-if (question3.toUpperCase() === 'YES' || === 'Y') {
+if (question3.toUpperCase() === 'YES' || question3.toUpperCase() === 'Y') {
 	alert('Hell yeah I can !!!');
 } else {
 	alert('Really? Humph. I\'m kind of offended.');
