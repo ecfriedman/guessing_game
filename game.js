4f79f5dc28043e587311ff40a041aeab8bb96c77
Emily Friedman <emilyfriedman@Emilys-MacBook-Pro.local>
2015-09-30 16:02:19 -0700

    added final message

--- game.js
+++ game.js
@@ -1,18 +1,22 @@
+var totals = 0;
 var question1 = prompt("Can I swim?");
 if (question1.toUpperCase() ==='YES' || question1.toUpperCase() === 'Y') {
 	alert('Correct! You never know when you are going to fall into the water.');
+	totals++;
 } else {
 	alert('Incorrect, sorry. That\'s a fundamental skill we ALL should know. No?');
 }
 var question2 = prompt("Can I fly?");
 if (question2.toUpperCase() === 'YES' || question2.toUpperCase() === 'Y') {
 	alert('That\'s right, I am magic !!!');
+	totals++;
 } else {
 	alert('Understandable mistake, but I just happen to be lucky like that.');
 }
 var question3 = prompt("Can I dance?")
 if (question3.toUpperCase() === 'YES' || question3.toUpperCase() === 'Y') {
 	alert('Hell yeah I can !!!');
+	totals++;
 } else {
 	alert('Really? Humph. I\'m kind of offended.');
 }
@@ -25,3 +29,4 @@ console.log("dancing response: " + question3),
 alert('your answer to "Can I swim?" was ' + question1);
 alert('your answer to "Can I fly?" was ' + question2);
 alert('your answer to "Can I dance?" was ' + question3);
+alert('You got ' + totals + 'correct! Thanks for playing');
