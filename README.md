c4674aceae8a5bb73d87ac6bbad7eeb1bec82e96
Robert Hill <codingbyrobert@gmail.com>
2015-10-01 13:34:29 -0700

    moved alerts and updated read me

---
+++ README.md
@@ -0,0 +1,4 @@
+Driver was Robert Hill
+Navigator was Emily Friedman
+
+We did work together on this. It was easy.
--- game.js
+++ game.js
@@ -3,31 +3,34 @@ var totals = 0;
 function q1() {
   var question1 = prompt("Can I swim?");
   if (question1.toUpperCase() ==='YES' || question1.toUpperCase() === 'Y') {
-    alert('Correct! You never know when you are going to fall into the water.');
+    one.innerHTML = 'Correct! You never know when you are going to fall into the water.';
     totals++;
   } else {
-    alert('Incorrect, sorry. That\'s a fundamental skill we ALL should know. No?');
+    one.innerHTML = 'Incorrect, sorry. That\'s a fundamental skill we ALL should know. No?';
   }
+  console.log("swimming response: " + question1);
 }
-
 function q2 () {
   var question2 = prompt("Can I fly?");
   if (question2.toUpperCase() === 'YES' || question2.toUpperCase() === 'Y') {
-    alert('That\'s right, I am magic !!!');
+   two.innerHTML = ' That\'s right, I am magic !!!';
     totals++;
   } else {
-    alert('Understandable mistake, but I just happen to be lucky like that.');
+    two.innerHTML = 'Understandable mistake, but I just happen to be lucky like that.';
   }
+  console.log("flying response: " + question2);
 }
 function q3 () {
     var question3 = prompt("Can I dance?")
   if (question3.toUpperCase() === 'YES' || question3.toUpperCase() === 'Y') {
-    alert('Hell yeah I can !!!');
+    three.innerHTML = 'Hell yeah I can !!!';
     totals++;
   } else {
-    alert('Really? Humph. I\'m kind of offended.');
+    three.innerHTML = 'Really? Humph. I\'m kind of offended.';
   }
+  console.log("dancing response: " + question3);
 }
+
 q1();
 q2();
 q3();
@@ -37,7 +40,8 @@ q3();
 // console.log("flying response: " + question2);
 // console.log("dancing response: " + question3),
 
+// not used anymore
 // alert('your answer to "Can I swim?" was ' + question1);
 // alert('your answer to "Can I fly?" was ' + question2);
 // alert('your answer to "Can I dance?" was ' + question3);
-// alert('You got ' + totals + 'correct! Thanks for playing');
+answers.innerHTML = 'You got ' + totals + ' correct! Thanks for playing.';
