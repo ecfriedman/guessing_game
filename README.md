3e0871f0153de6009f3154481920982f71d9c323
David Flegal <flegal.david@gmail.com>
2015-10-02 13:37:17 -0700

    Added style and flair

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ Driver was Robert Hill
 Navigator was Emily Friedman
 
 We did work together on this. It was easy.
+
+
+10/2
+Driver: David Flegal
+Navigator: Emily Friedman
--- game.js
+++ game.js
@@ -5,33 +5,33 @@ var userInput = ['YES', 'Y'];
 function q1() {
   var question1 = prompt(quests[0]);
   if (question1.toUpperCase() === userInput[0] || question1.toUpperCase() === userInput[1]) {
-    one.innerHTML = 'Correct! You never know when you are going to fall into the water.';
-    pic1.innerHTML = '<img src="img/Swimming-baby1.jpg" width="200"/>';
+    oneY.innerHTML = 'Correct! You never know when you are going to fall into the water.';
+    pic1.innerHTML = '<img src="images/Swimming-baby1.jpg" width="200"/>';
     totals++;
   } else {
-    one.innerHTML = 'Incorrect, sorry. That\'s a fundamental skill we ALL should know. No?';
+    oneN.innerHTML = 'Incorrect, sorry. That\'s a fundamental skill we ALL should know. No?';
   }
   console.log("swimming response: " + question1);
 }
 function q2 () {
   var question2 = prompt(quests[1]);
   if (question2.toUpperCase() === userInput[0] || question2.toUpperCase() === userInput[1]) {
-   two.innerHTML = ' That\'s right, I am magic !!!';
-   pic2.innerHTML = '<img src = "img/flying-dream.jpg" width="200"/>';
+   twoY.innerHTML = ' That\'s right, I am magic !!!';
+   pic2.innerHTML = '<img src = "images/flying-dream.jpg" width="200"/>';
     totals++;
   } else {
-    two.innerHTML = 'Understandable mistake, but I just happen to be lucky like that.';
+    twoN.innerHTML = 'Understandable mistake, but I just happen to be lucky like that.';
   }
   console.log("flying response: " + question2);
 }
 function q3 () {
     var question3 = prompt(quests[2]);
   if (question3.toUpperCase() === userInput[0]  || question3.toUpperCase() === userInput[1]) {
-    three.innerHTML = 'Hell yeah I can !!!';
-    pic3.innerHTML = '<img src = "img/dancing.jpg" width="200"/>';
+    threeY.innerHTML = 'Hell yeah I can !!!';
+    pic3.innerHTML = '<img src = "images/dancing.jpg" width="200"/>';
     totals++;
   } else {
-    three.innerHTML = 'Really? Humph. I\'m kind of offended.';
+    threeN.innerHTML = 'Really? Humph. I\'m kind of offended.';
   }
   console.log("dancing response: " + question3);
 }
